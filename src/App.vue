ff5b05f50f18b90d2f7074134047bc0428ee0fff
Ken Dong <yid164@mail.usask.ca>
2020-03-15 14:14:20 -0600

    Setup the layout, and link the router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
   <v-app>
-    <v-content transition="slide-x-transition">
-      <v-btn to="/">Home</v-btn>|
-      <router-link to="/profile">Profile</router-link> |
-      <router-link to="/skills">Skills</router-link> |
-      <router-link to="/experience">Experience</router-link> |
-      <router-link to="/projects">Projects</router-link> |
-      <router-link to="/contact">Contact</router-link>
-      <router-view></router-view>
+    <v-navigation-drawer app permanent>
+      <v-list-item>
+        <v-list-item-title class="title">
+          Ken
+        </v-list-item-title>
+        <v-list-item-subtitle class="subtitle-2">
+          A job seeker
+        </v-list-item-subtitle>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" router :to="item.to">
+          <v-list-item-icon>
+            <v-icon :to=item.to>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title> {{ item.title }} </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+        app-bar
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+
+      <v-footer>
+
+      </v-footer>
     </v-content>
   </v-app>
+
 </template>
 
 <script>
-
-
 
 export default {
   name: 'App',
   components: {
   },
-  data: () => ({
-  }),
-};
+  data () {
+    return{
+      items: [
+        {title: 'Home', icon: 'mdi-view-dashboard', to: '/'},
+        {title: 'Profile', icon: 'mdi-view-dashboard', to: '/profile'},
+        {title: 'Skills', icon: 'mdi-view-dashboard', to: '/skills'},
+        {title: 'Projects', icon: 'mdi-view-dashboard', to: '/project'},
+        {title: 'Experience', icon: 'mdi-view-dashboard', to: '/experience'},
+        {title: 'Contacts', icon: 'mdi-view-dashboard', to: '/contact'}
+
+      ]
+    }
+  }
+}
 </script>
